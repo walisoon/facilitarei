e6d754759879ec37f1ef8f309a857f58b4d20dce
walisoon <iadoagro123@gmail.com>
2025-02-05 17:10:39 -0400

    Atualizado credenciais do Supabase

--- js/auth.js
+++ js/auth.js
@@ -1,6 +1,6 @@
 // Inicializar o cliente Supabase
-const SUPABASE_URL = 'https://euphonious-selkie-17c6d9.netlify.app';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...'; // Substitua pela sua chave anônima
+const SUPABASE_URL = 'https://qkimxruewcensbnfllvv.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFraW14cnVld2NlbnNibmZsbHZ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzMwOTIxMzksImV4cCI6MjA0ODY2ODEzOX0.tLnz1o3ximSJFMHb1kRzbfmF-4gIP_i-YD6n5TH24fE';
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 // Função para lidar com o login
